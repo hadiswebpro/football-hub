c1c19ae2873482ba9fdd306d9d213d5c64d35462
Hadis <hadiswebpro@gmail.com>
2026-09-04 01:04:31 +0330

    Import Football Hub styles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import "./styles/main.css";
+import "./styles/header.css";
+import "./styles/cards.css";
+import "./styles/responsive.css";
+
 import createHeader from "./components/header";
 import loadCompetitions from "./pages/home";
 
