fa24c6d9d16a05b71c88a062c956f74522154456
Hadis <hadiswebpro@gmail.com>
2026-09-05 01:07:26 +0330

    feat: add hash routing for match page

--- src/index.js
+++ src/index.js
@@ -2,10 +2,23 @@ import "./styles/main.css";
 import "./styles/header.css";
 import "./styles/hero.css";
 import "./styles/cards.css";
+import "./styles/match-page.css";
 import "./styles/responsive.css";
 
 import createHeader from "./components/header";
 import loadCompetitions from "./pages/home";
+import createMatchPage from "./pages/match";
 
 createHeader();
-loadCompetitions();
+
+function renderRoute() {
+    if (window.location.hash === "#match") {
+        createMatchPage();
+        return;
+    }
+
+    loadCompetitions();
+}
+
+window.addEventListener("hashchange", renderRoute);
+renderRoute();
