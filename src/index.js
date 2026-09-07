fdcb43904c8ca4283552e1c183e75e680373d895
Hadis <hadiswebpro@gmail.com>
2026-09-07 21:05:42 +0330

    feat: add league page routing

--- src/index.js
+++ src/index.js
@@ -3,17 +3,24 @@ import "./styles/header.css";
 import "./styles/hero.css";
 import "./styles/cards.css";
 import "./styles/match-page.css";
+import "./styles/league-page.css";
 import "./styles/responsive.css";
 
 import createHeader from "./components/header";
 import loadCompetitions from "./pages/home";
 import createMatchPage from "./pages/match";
+import createLeaguePage from "./pages/league";
 
 createHeader();
 
 function renderRoute() {
     if (window.location.hash === "#match") {
         createMatchPage();
+        return;
+    }
+
+    if (window.location.hash === "#league") {
+        createLeaguePage();
         return;
     }
 
