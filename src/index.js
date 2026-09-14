333e78e7191e4d77880a856f6b51e78ade8e988f
Hadis <hadiswebpro@gmail.com>
2026-09-15 01:33:04 +0330

    Load shared UI polish styles

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./styles/header.css";
 import "./styles/hero.css";
 import "./styles/cards.css";
 import "./styles/logo-cards.css";
+import "./styles/ui-polish.css";
 import "./styles/match-page.css";
 import "./styles/matches-page.css";
 import "./styles/league-page.css";
